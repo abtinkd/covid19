758dfc54a92c1536e47d5a9169e432f5e748a63b
Abt@Win <abtinkh+github@gmail.com>
2020-06-08 23:49:31 -0700

    update - shock index

--- helper_functions.py
+++ helper_functions.py
@@ -1,10 +1,26 @@
 import datetime
 import pandas as pd
 
+def load_data(url, dtype=None):
+    import os
+    from six.moves import urllib
+    _, filename = url.rsplit('/', 1)
+    filename = str(datetime.date.today()) + '_' + filename
+    filepath = 'datasets/' + filename
+    if not os.path.isdir('datasets'):
+        os.makedirs('datasets')
+    try:
+        if not os.path.exists(filepath):
+            urllib.request.urlretrieve(url, filepath)
+    except:
+        print('Could not retrieve ' + url)
+        raise
+    return pd.read_csv(filepath, dtype=dtype)    
+
 def load_main_dataframe(date=None):
     if date is None:
         date = datetime.datetime.today().date()
-    return (pd.read_csv(f'datasets/{date}_covid19_stats.csv', keep_default_na=False, float_precision='round_trip',
+    return (pd.read_csv(f'datasets/{date}_covid19_stats.csv', float_precision='round_trip',
                         index_col=['country_region', 'province_state', 'county', 'date'], 
                         dtype={'country_region':str, 'province_state':str, 'county':str},
                         parse_dates=True)
